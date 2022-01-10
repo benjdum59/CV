226fd7710907a83c4b72b46de751ed96d4718236
Benjamin DUMONT <benjamin.dumont@axa.fr>
2022-01-10 09:09:55 +0100

    import project

---
+++ CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by Benjamin DUMONT on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
